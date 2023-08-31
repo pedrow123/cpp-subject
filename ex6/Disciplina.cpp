c10f61a53eb08b9833fd52677cf26f2beaad0a91
pwa21 <pwa21@inf.ufpr.br>
2023-08-31 09:35:49 -0300

    ex7

--- ex6/Disciplina.cpp
+++ ex6/Disciplina.cpp
@@ -5,10 +5,7 @@ Disciplina::Disciplina(std::string nome, Curso& cursoVinculado)
 
 Disciplina::Disciplina(std::string nome, Pessoa* professor,
                        Curso& cursoVinculado)
-    : nome{nome},
-      professor{professor},
-      cursoVinculado{cursoVinculado},
-      qtdeAlunos{0} {}
+    : nome{nome}, professor{professor}, cursoVinculado{cursoVinculado} {}
 
 std::string Disciplina::getNome() { return this->nome; }
 
@@ -30,21 +27,43 @@ std::string Disciplina::getNomeProfessor() {
     return this->professor->getNome();
 }
 
-bool Disciplina::adicionarAluno(Pessoa* aluno) {
-    if (getQtdeAlunos() < 50) {
-        this->alunos[getQtdeAlunos()] = aluno;
-        setQtdeAlunos(getQtdeAlunos() + 1);
+bool Disciplina::adicionarAluno(Pessoa* aluno) { 
+    if (getVetorAlunos().size() < 50){
+        getVetorAlunos().push_back(aluno);
         return true;
     }
     return false;
 }
 
-int Disciplina::getQtdeAlunos() { return this->qtdeAlunos; }
-
-void Disciplina::setQtdeAlunos(unsigned short int qtdeAlunos) {
-    this->qtdeAlunos = qtdeAlunos;
+bool Disciplina::removerAluno(Pessoa* aluno){
+    if(getVetorAlunos().size() > 0){
+        getVetorAlunos().remove(aluno);
+        return true;
+    }
+    return false;
 }
 
-Pessoa** Disciplina::getVetorAlunos() { return this->alunos; }
+bool Disciplina::removerAluno(unsigned long int cpf){
+    std::list<Pessoa*>::iterator it;
+    for(it = getVetorAlunos().begin(); it != getVetorAlunos().end(); ++it){
+        if((*it)->getCpf() == cpf)
+            break;
+    }
+    if(it != getVetorAlunos().end()){
+        Pessoa* ptr{*it};
+        getVetorAlunos().erase(it);
+        delete ptr;
+        return true;
+    }
+    return false;
+}
+
+// int Disciplina::getQtdeAlunos() { return this->qtdeAlunos; }
+
+// void Disciplina::setQtdeAlunos(unsigned short int qtdeAlunos) {
+//     this->qtdeAlunos = qtdeAlunos;
+// }
+
+std::list<Pessoa*> Disciplina::getVetorAlunos() { return this->alunos; }
 
 Curso& Disciplina::getCursoVinculado() { return this->cursoVinculado; }
